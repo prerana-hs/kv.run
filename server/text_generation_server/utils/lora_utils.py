75099d6a93ac10d71ff31178e4c7cb78915233ab
MichaelYuan2 <104136162+MichaelYuan2@users.noreply.github.com>
2024-04-01 21:10:58 +0800

    Update README.md

--- server/text_generation_server/utils/lora_utils.py
+++ server/text_generation_server/utils/lora_utils.py
@@ -323,7 +323,6 @@ class ModelLoraManager:
     ):
         for lora_id in lora_ids:
             if lora_id not in self.lora_weights_cpu:
-<<<<<<< HEAD
                 model_path, config_path = load_lora_weights(lora_id)
                 raw_weights = torch.load(
                     model_path, map_location="cpu", weights_only=True
@@ -334,25 +333,11 @@ class ModelLoraManager:
                     if lora_rank < 16
                     else ModelLoraWeight(model_config, lora_rank, dtype, "cpu")
                 )
-=======
-                # model_path, config_path = load_lora_weights(lora_id)
-                model_path, config_path = load_lora_weights_local(lora_id)
-                raw_weights = torch.load(model_path, map_location='cpu', weights_only=True)
-                lora_rank = peft.config.PeftConfigMixin.from_json_file(config_path)['r']
-                lora_weight = ModelLoraWeight(model_config, lora_rank*2, dtype, 'cpu') \
-                              if lora_rank < 16 \
-                              else ModelLoraWeight(model_config, lora_rank, dtype, 'cpu')
->>>>>>> ea7b3b1 (.)
                 converted_weights = self.__convert_weight(raw_weights, lora_rank)
                 lora_weight.copy_from_tensors(converted_weights)
                 del converted_weights
                 self.lora_weights_cpu[lora_id] = lora_weight
-<<<<<<< HEAD
                 logger.info(f"{lora_id} loaded in cpu memory!")
-=======
-                print(self.lora_weights_cpu)
-                logger.info(f'{lora_id} loaded in cpu memory!')
->>>>>>> ea7b3b1 (.)
 
     def remove_lora_weights(self, lora_ids: List[str] = None):
         if (not lora_ids) or (lora_ids == "") or (lora_ids == "all"):
