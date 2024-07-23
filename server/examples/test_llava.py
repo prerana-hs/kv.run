48eb90af063c99fb5e5a033ebb1cb518a9b04cb1
J1shen <jyshen44@gmail.com>
2024-07-23 11:03:18 +0800

    path fix

--- server/examples/test_llava.py
+++ server/examples/test_llava.py
@@ -1,5 +1,5 @@
 from text_generation_server.pb import generate_pb2_grpc, generate_pb2
-from text_generation_server.models.llava_flashinfer_lm import LlavaLM, LlavaBatch
+from text_generation_server.models_flashinfer.llava_flashinfer import LlavaLM, LlavaBatch
 import random, torch
 import base64
 
