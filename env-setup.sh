f840cd56068b30d18584d16715bfdad43b64f78a
prerana <prerana.chaks@gmail.com>
2024-08-24 10:13:04 +0800

    installing pkg-config

--- env-setup.sh
+++ env-setup.sh
@@ -2,6 +2,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
 sudo apt-get install unzip
+sudo apt install pkg-config
 sudo apt-get install libssl-dev gcc -y
 PROTOC_ZIP=protoc-21.12-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v21.12/$PROTOC_ZIP
